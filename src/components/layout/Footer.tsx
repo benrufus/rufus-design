20b1a93f6148fb18ffa8555e2517d311b3671d14
benrufus <ben@rufusdesign.co.uk>
2026-06-12 11:42:11 +0100

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -115,7 +115,7 @@ export default function Footer() {
             )}
           </ul>
         </div>
-        <div class="menu-dog"><img src="/RufusDoggo.png" alt="Rufus" style="width:220px;height:220px;object-fit:cover"></div>
+        <div className="menu-dog"><img src="/RufusDoggo.png" alt="Rufus" style="width:220px;height:220px;object-fit:cover"></div>
       </div>
 
       <div className="footer-bottom">
