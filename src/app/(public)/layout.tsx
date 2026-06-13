4b44eeead064b70b8598f0b9d07d6673f19a6d20
benrufus <ben@rufusdesign.co.uk>
2026-06-13 08:33:36 +0100

    Update layout.tsx

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,16 @@
+'use client'
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 import Nav from '@/components/layout/Nav'
 import Footer from '@/components/layout/Footer'
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
     <>
       <Nav />
